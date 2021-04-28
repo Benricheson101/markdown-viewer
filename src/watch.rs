4a1f0dab8e65d46e29a053e21dbefae4c46250cd
Benjamin Richeson <benricheson101@gmail.com>
2021-04-27 22:27:01 -0400

    o

--- src/watch.rs
+++ src/watch.rs
@@ -6,57 +6,6 @@ use std::{
 
 use notify::{RawEvent, RecommendedWatcher, RecursiveMode, Watcher};
 
-// pub fn watch<P: Into<PathBuf> + Clone>(
-//     path: P,
-// ) -> notify::Result<(Receiver<RawEvent>, PathBuf)> {
-// pub fn watch<P>(tx: Sender<RawEvent>, path: P) -> notify::Result<()>
-// where
-//     P: Clone + Into<PathBuf>,
-// {
-//     let path: PathBuf = path.into();
-//     let path = fs::canonicalize(path)?;
-
-//     // let (send, recv) = mpsc::channel();
-//     let mut watcher: RecommendedWatcher = Watcher::new_raw(tx)?;
-
-//     if path.is_dir() {
-//         panic!("cannot watch directory");
-//     }
-
-//     // required for compatibility with some editors.
-//     //
-//     // instead of writing directly to the file, some editors write to a
-//     // temporary file, remove the original file and then move the temp file
-// to     // the location of the original file. in vim, this behavior is
-// controlled     // under the `backupcopy` option
-//     let dir = path.parent().unwrap();
-
-//     println!("watching {}", dir.display());
-//     watcher.watch(dir, RecursiveMode::NonRecursive)?;
-
-//     Ok(())
-
-//     // loop {
-//     //     match rx.recv() {
-//     //         Ok(RawEvent {
-//     //             op: Ok(Op::WRITE),
-//     //             path: Some(file_written),
-//     //             ..
-//     //         }) if file_written == path => {
-//     //             println!("wrote file!");
-//     //         },
-//     //         _ => {},
-//     //     }
-//     // }
-
-//     // loop {
-//     //     let msg = recv.recv().unwrap();
-//     //     dbg!(&msg);
-
-//     //     tx.send(msg).unwrap();
-//     // }
-// }
-
 // TODO: figure out why channels werent working here
 
 /// Watch a file for updates with `notify`
